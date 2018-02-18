539d7a97f47a8bdb9b6f11abbe167d70ddb92aaa
evan1533 <evan1533@vt.edu>
2018-02-18 02:06:04 -0500

    asdf

--- MT/notemaker.py
+++ MT/notemaker.py
@@ -6,5 +6,5 @@ for i in range(2400):
     NoteString = ""
     for k in range(1,random.randint(1,4)):
         NoteString = NoteString + poss[random.randint(0,len(poss)-1)]
-    notesAnim.append(NoteString)
+    notesAnim.append(NoteString )
 print(notesAnim)
