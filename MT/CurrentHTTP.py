1f65fba5cfa66a33c9323b6d478ff3438256ec1c
evan1533 <evan1533@vt.edu>
2018-02-19 17:08:32 -0500

    More

--- MT/CurrentHTTP.py
+++ MT/CurrentHTTP.py
@@ -11,9 +11,11 @@ import re
 from io import BytesIO
 
 class Handler(BaseHTTPRequestHandler):
+    globalFileName = "";
+    
     def do_GET(self):
         f = self.send_head()
-        myFile = open("GestTema.txt","rb");
+        #myFile = open("GestTema.txt","rb");
         if f:
             self.copyfile(f, self.wfile)
             f.close()
@@ -24,11 +26,49 @@ class Handler(BaseHTTPRequestHandler):
         form = cgi.FieldStorage(
             fp=self.rfile, 
             headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
+            environ={'CONTENT_TYPE':self.headers['Content-Type'],
                      })
-        themood = str(form["HID"].value).strip()
-        return
+        f = BytesIO()
+        f.write(bytes("""
+<!DOCTYPE html>
+<html>
+<head>
+  <script>
+      function playAudio3() {
+    var music = document.getElementById('music1019');
+    music.play();
+}
+  </script>
+  <meta charset="UTF-8">
+  <title>The Three W's</title>
+  <link rel="stylesheet" type="text/css" href="path to css stylesheet">
+</head>
+<body>
+<audio id="music1019" autobuffer="false" preload="none" title="Play the AM piano chord">""","utf8"))
+        f.write(bytes("""<source id="Sus" src="http://172.24.66.145:8090/%s" type="audio/wav">""" % self.globalFileName,"utf8"))
+        f.write(bytes("""
+</audio>
+  <h1>
+    The Three W's
+  </h1>
+  <h2>
+    Word, Were, Work.
+  </h2>
+    <div>
+    <button onclick="playAudio3()">PLAY</button>
+  </div>
+</body>
+</html>
+""","utf8"))
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        if f:
+            self.copyfile(f, self.wfile)
+            f.close()
 
     def send_head(self):
         """Common code for GET and HEAD commands.
@@ -47,7 +87,7 @@ class Handler(BaseHTTPRequestHandler):
                 self.send_header("Location", self.path + "/")
                 self.end_headers()
                 return None
-            for index in "index.html", "index.htm":
+            for index in "uploadPage.html", "index.htm":
                 index = os.path.join(path, index)
                 if os.path.exists(index):
                     path = index
@@ -122,6 +162,50 @@ class Handler(BaseHTTPRequestHandler):
         """
         shutil.copyfileobj(source, outputfile)
 
+    def deal_post_data(self):
+        content_type = self.headers['content-type']
+        if not content_type:
+            return (False, "Content-Type header doesn't contain boundary")
+        boundary = content_type.split("=")[1].encode()
+        remainbytes = int(self.headers['content-length'])
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        if not boundary in line:
+            return (False, "Content NOT begin with boundary")
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        fn = re.findall(r'Content-Disposition.*name="file"; filename="(.*)"', line.decode())
+        if not fn:
+            return (False, "Can't find out file name...")
+        path = self.translate_path(self.path)
+        self.globalFileName = fn[0]
+        fn = os.path.join(path, fn[0])
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        try:
+            out = open(fn, 'wb')
+        except IOError:
+            return (False, "Can't create file to write, do you have permission to write?")
+                
+        preline = self.rfile.readline()
+        remainbytes -= len(preline)
+        while remainbytes > 0:
+            line = self.rfile.readline()
+            remainbytes -= len(line)
+            if boundary in line:
+                preline = preline[0:-1]
+                if preline.endswith(b'\r'):
+                    preline = preline[0:-1]
+                out.write(preline)
+                out.close()
+                return (True, "File '%s' upload success!" % fn)
+            else:
+                out.write(preline)
+                preline = line
+        return (False, "Unexpect Ends of data.")
+
     if not mimetypes.inited:
         mimetypes.init() # try to read system mime.types
     extensions_map = mimetypes.types_map.copy()
@@ -132,5 +216,5 @@ class Handler(BaseHTTPRequestHandler):
         '.h': 'text/plain',
         })
         
-server = HTTPServer(('localhost', 8090), Handler)
+server = HTTPServer(('172.24.66.145', 8090), Handler)
 server.serve_forever()
